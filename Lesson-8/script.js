56e0f2f594196574fe7b20c74aefd521b2ef6016
Siam Abdullah <mrsiamabdullah@gmail.com>
2024-05-16 19:42:41 +0600

    Lesson - 8 and 9 added

---
+++ Lesson-8/script.js
@@ -0,0 +1,44 @@
+/* alert("Important!");
+document.write("I love bangladesh"); */
+console.log("Hello World!");
+
+var name1, name2, name3, testNumber, newName, number1;
+
+name1 = "Siam Abdullah";
+newName = "Sinan Abdullah";
+name2 = "Nafiz Abdullah";
+name3 = "Jubayer Abdullah";
+(testNumber = "17.5"), (testNumber = parseFloat(testNumber));
+number1 = (17.765649).toFixed(3);
+
+console.log(newName);
+console.log(name1);
+console.log(name2);
+console.log(name3);
+console.log(typeof testNumber);
+console.log(number1);
+console.log(typeof Number("17"));
+
+// String Concatination
+var firstName = "Siam";
+var lastName = " Abdullah";
+var fullName = firstName.concat(lastName);
+
+console.log(fullName);
+console.log("My name is " + firstName + " " + lastName);
+
+// Library functions for string
+var wordLength = "I love Bangladesh";
+console.log(wordLength.length);
+
+// var userPrompt = prompt("Enter your text here");
+// var userNumberPrompt = prompt("Enter your text character number");
+
+// document.write("Your text length is: " + userPrompt.length, "</br>");
+
+// letterCount = userPrompt.charAt(userNumberPrompt);
+// document.write("Your text character letter: " + letterCount);
+
+var toUppercase = "Bangladesh";
+toUppercase = toUppercase.toUpperCase();
+console.log(toUppercase);
